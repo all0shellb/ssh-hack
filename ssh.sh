7425e9c86c12a28d04f0e5a1af7192f813fae71c
all0shellb <32724773+all0shellb@users.noreply.github.com>
2017-10-12 10:38:07 +0800

    Add files via upload

---
+++ ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#all0shell.org
+cat /var/log/secure|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"=" $1;}' > /tmp/hack-ssh.txt //查看Linux登录日志 输出
+DEFINE="10"//登录错误10次
+for i in `cat /tmp/hack-ssh.txt`
+do 
+        IP=`echo $i|awk -F= '{print $1}'`
+        NUM=`echo $i|awk -F= '{print $2}'`
+        if [ $NUM -gt $DEFINE ]
+        then
+                grep $IP /etc/hosts.deny >/dev/null //看看ip是否存在
+                if [ $? -gt 0 ]; //若不存在，则执行下面的语句
+                then
+                echo "sshd:$IP" >> /etc/hosts.deny 
+                fi
+        fi
+done
